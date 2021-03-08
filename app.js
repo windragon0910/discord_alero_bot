12867482d0b2f9a85ccb40eba7c16a73f45a386f
Windragon <windragon0910@gmail.com>
2021-03-08 13:42:16 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const Discord = require('discord.js');
 const colors = require('colors');
 const fs = require('fs');
 
+const memberCounter = require('./server/counters/member-counter');
+
 /* Config */
 const Config = require('./server/config/config.json');
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 client.commands = new Discord.Collection();
 
 /* Commands */
@@ -24,13 +26,14 @@ for (const folder of commandFolders) {
 
 client.once('ready', () => {
     console.log(`Bot started. Name: ${client.user.tag}`.bgRed.white);
+    memberCounter(client)
 });
 
-
+client.on('error', console.error);
 client.on('message', message => {
     /* Log messages sent */
     if(!message.author.bot) {
-        console.log(`${message.content}`.green);
+        console.log(`MESSAGE SENT: ${message.content}`.bgWhite.green);
     };
     
     /* Command Set-Up */
@@ -41,22 +44,66 @@ client.on('message', message => {
     
     function exCommand(str) {
         client.commands.get(str).execute(message, args);
-        console.log(`${str} executed.`.toUpperCase().bgBlue.red);
+        console.log(`${str} executed.`.toUpperCase().bgRed.white);
     };
+    function exCommand2(str) {
+        client.commands.get(str).execute(message, args, Discord, client);
+        console.log(`${str} executed.`.toUpperCase().bgWhite.red);
+    }
 
     switch(command) {
+            /* Utility */
         case 'ping':
             exCommand('ping');
             break;
+            /* Information */
         case 'dev-info':
             exCommand('dev-info');
             break;
-    }
+            /* Moderation */
+        case 'clear':
+            exCommand('clear');
+            break;
+        case 'kick':
+            exCommand('kick');
+            break;
+        case 'ban':
+            exCommand('ban');
+            break;
+        case 'mute':
+            exCommand('mute');
+            break;
+        case 'unmute':
+            exCommand('unmute');
+            break;
+            /* Reaction Roles */
+        case 'reactionrole':
+            exCommand2('reactionrole');
+            break;
+            /* Music */
+        case 'clearqueue':
+            exCommand('clearqueue');
+            break;
+        case 'play':
+            exCommand('play');
+            break;
+        case 'disconnect':
+            exCommand('disconnect');
+            break;
+    };
     if(!command) return;
     
     if(command.guildOnly && message.channel.type === 'dm') {
         return message.reply('I can\'t execute that command inside DM\'s');
     };
+});
+
+client.on('guildMemberAdd', guildMember => {
+    console.log(`MEMBER JOINED: ${guildMember}`.green);
+    let mainRole = guildMember.guild.roles.cache.find(role => role.name === 'Member');
+
+    guildMember.roles.add(mainRole);
+    guildMember.guild.channels.cache.get('816740392916615209').send(`Welcome <@${guildMember.user.id}> to our server, make sure to check out the rule text channel!`);
 });
 
 client.login(Config.token);
