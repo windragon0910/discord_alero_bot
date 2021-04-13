9ff597b81ad616df70eb0476fb8551fc4d31b3a8
Windragon <windragon0910@gmail.com>
2021-04-13 00:16:40 -0300

    Delete server directory

--- server/commands/music/clearqueue.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'clearqueue',
-    description: "in progress, clear all the queue of songs.",
-    execute(message, args) {
-        
-    }
-}
--- server/commands/utility/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'This is a Ping command!',
-    execute(message, args) {
-        message.channel.send('🏓 Pong!');
-    },
-};
